27c25e8fa6f54774577febd7a72ef6159d2f42ad
geral <geral@192.168.217.131>
2018-02-22 13:28:33 -0300

    SISTEMA POS 09 15:35

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,54 @@
+<header class="main-header">
+    <!--=============================================
+    LOGOTIPO
+    ==============================================-->
+    <a href="" class="logo">
+        <!--logo mini-->
+        <span class="logo-mini">
+            <img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive" style="padding: 10px">
+        </span>
+
+
+        <!--logo normal-->
+        <span class="logo-lg">
+            <img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive" style="padding: 10px 0px">
+        </span>
+    </a>
+
+
+
+
+    <!--=============================================
+    BARRA DE NAVAGACAO
+    ==============================================-->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <!--BOTAO DE NAVEGACAO-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+<!--            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>-->
+        </a>
+        
+        
+        <!--PERFIL DE USUARIO-->
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="vistas/img/usuarios/default/anonymous.png" class="user-image">
+                        <span class="hidden-xs">Usuario administrador</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        
+        
+        
+        
+        <!--DROPDOWN-TOGGLE-->
+
+
+    </nav>
+
+</header>

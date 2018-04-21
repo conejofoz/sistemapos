13325f98c3266a8fb4b2942dd4fdec40b7dbe8f0
geral <geral@192.168.217.131>
2018-04-21 11:25:37 -0300

    maquetando el modulo de usuarios p2

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -63,9 +63,9 @@
 
 
             <!--====================RODAPE================================-->
-            
+
         </div>
- 
+
 
     </section>
 </div>
@@ -78,21 +78,74 @@
 
 <!-- MODAL AGREGAR USUARIO -->
 <div id="modalAgregarUsuario" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+    <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
+        <div class="modal-content">
+            <form role="form" method="POST" enctype="multipart/form-data">
+
+
+                <div class="modal-header" style="background: #3c8dbc; color: white">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Agregar usuario</h4>
+                </div>
+
+
+                <div class="modal-body">
+                    <div class="box-body">
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                                <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Ingresar usuario" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar contraseña" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                                <select class="form-control input-lg" name="nuevoPerfil">
+                                    <option value="">Selecionar perfil</option>
+                                    <option value="Administrador">Administrador</option>
+                                    <option value="Especial">Especial</option>
+                                    <option value="Vendedor">Vendedor</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="panel text-uppercase">SUBIR FOTO</div>
+                            <input type="file" id="nuevaFoto" name="nuevaFoto">
+                            <p class="help-block">Peso máximo de la foto 200MB</p>
+                            <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px">
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+                    <button type="submit" class="btn btn-primary">Guardar usuario</button>
+                </div>
+
+
+            </form>
+        </div>
+
     </div>
-
-  </div>
 </div>
